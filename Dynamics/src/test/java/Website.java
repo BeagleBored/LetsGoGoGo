7b2267418c7ec9c5dcc1dce7ac2c8881a707be5e
josh <josh@joshuas-mini.lan>
2022-05-16 18:52:48 -0500

    Everything works.

--- Dynamics/src/test/java/Website.java
+++ Dynamics/src/test/java/Website.java
@@ -1,4 +1,34 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class Website {
+
+
+	private WebDriver driver;
+
+	String chromeDriverPath = "/src/test/resources/mac/chromedriver";
+	String projectPath = System.getProperty("user.dir");
+	protected String baseUrl = "https://letsgo.com";
+	
+	
+	@Test
+	public void LetsGOOO() {
+		System.setProperty("webdriver.chrome.driver", projectPath + chromeDriverPath);
+		this.driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get(baseUrl);	
+
+		//WebElement clickSignInButton = driver.findElement(By.cssSelector("a[class='nav__button-secondary']"));
+		//clickSignInButton.click();
+
+	}
+	
+
 
 }
